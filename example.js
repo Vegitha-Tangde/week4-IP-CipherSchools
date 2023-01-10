9ab01cfc03f01ce83781621fca2c91b3caf9f45d
Vegitha-Tangde <118879606+Vegitha-Tangde@users.noreply.github.com>
2023-01-11 02:00:21 +0530

    Add files via upload

---
+++ example.js
@@ -0,0 +1,36 @@
+// Datatypes
+
+// Syntax and declaration
+
+var score = 10;  // ESS - Ecma
+
+let result = 50; // ES6 - Ecmascript 2015
+const PI = 3.14; // ES6
+
+
+score = 15;
+result = 55;
+PI = 3.26;
+
+console.log(score);
+console.log(result);
+console.log(PI);
+
+
+/**
+ * Datatypes
+ * 1. Number
+ * 2. Strings
+ * 3. Boolean
+ * 4. Null
+ * 5. Undefined
+ * 6. Objects
+ * 7. Symbols (es6)
+ * 
+ * Loosely Typed
+ */
+
+let score = 10;  // Number
+let result = "10"; // String
+
+console.log(score, result);
